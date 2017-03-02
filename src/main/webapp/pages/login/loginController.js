d6855e497123b904b7cc11567ef054e53fc7eadc
Safayat Rahaman <safayat@cefalo.com>
2017-03-02 17:23:14 +0600

    login

--- src/main/webapp/pages/login/loginController.js
+++ src/main/webapp/pages/login/loginController.js
@@ -4,6 +4,65 @@ myApp.controller('loginController', function($scope, $http, appConfig,$rootScope
 	$rootScope.hideHeader = true;
 	$rootScope.hideSideBar = true;
 
+    var auth = encode("rajithapp:secret");
+    console.log(appConfig);
+    $http({
+        url: appConfig.baseUrl + '/oauth/token?username=admin&password=admin&grant_type=password',
+        method: "GET",
+        headers: {'Content-Type': 'application/json',"Authorization": "Basic " + auth
+        }
+
+    }).success(function(data){
+            if(successCallback){
+                successCallback(data);
+            }
+
+        }).error(function(data, status){
+            console.log("error:" + data);
+            if(data == 'USER_IS_UN_AUTHORIZED'){
+                $location.path('/login');
+            }
+
+            if(errorCallback){
+                errorCallback(data, status);
+            }
+        });
+
+    function encode (input) {
+        var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+        var output = "";
+        var chr1, chr2, chr3 = "";
+        var enc1, enc2, enc3, enc4 = "";
+        var i = 0;
+
+        do {
+            chr1 = input.charCodeAt(i++);
+            chr2 = input.charCodeAt(i++);
+            chr3 = input.charCodeAt(i++);
+
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+
+            output = output +
+                keyStr.charAt(enc1) +
+                keyStr.charAt(enc2) +
+                keyStr.charAt(enc3) +
+                keyStr.charAt(enc4);
+            chr1 = chr2 = chr3 = "";
+            enc1 = enc2 = enc3 = enc4 = "";
+        } while (i < input.length);
+
+        return output;
+    }
+
 
 
 });
